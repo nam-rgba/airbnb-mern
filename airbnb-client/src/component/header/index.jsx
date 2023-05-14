0c1e99750d03f9e0594b2d394103d32445b29643
Nam <doanngocnam3002@gmail.com>
2023-05-14 14:46:48 +0700

    set up

---
+++ airbnb-client/src/component/header/index.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className="logo">
+          <img src="" alt="" />
+        </div>
+      </header>
+    </>
+  );
+}
