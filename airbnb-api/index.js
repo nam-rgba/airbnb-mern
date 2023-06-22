99fa8ddecbccfd6d23829ffed1c656aba8d700e4
Nam <doanngocnam3002@gmail.com>
2023-06-22 23:36:41 +0700

    api upd

--- airbnb-api/index.js
+++ airbnb-api/index.js
@@ -98,4 +98,34 @@ app.post("/logout", (req, res) => {
   res.cookie("token", "").json();
 });
 
+app.post("/api/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
+  await imageDownloader.image({
+    url: link,
+    dest: "/tmp/" + newName,
+  });
+  const url = await uploadToS3(
+    "/tmp/" + newName,
+    newName,
+    mime.lookup("/tmp/" + newName)
+  );
+  res.json(url);
+});
+
+const photosMiddleware = multer({ dest: "/tmp" });
+app.post(
+  "/api/upload",
+  photosMiddleware.array("photos", 100),
+  async (req, res) => {
+    const uploadedFiles = [];
+    for (let i = 0; i < req.files.length; i++) {
+      const { path, originalname, mimetype } = req.files[i];
+      const url = await uploadToS3(path, originalname, mimetype);
+      uploadedFiles.push(url);
+    }
+    res.json(uploadedFiles);
+  }
+);
+
 app.listen(4000);
