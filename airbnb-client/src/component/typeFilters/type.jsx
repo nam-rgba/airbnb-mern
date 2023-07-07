4376f2d8e976553084f51c1abfc095fd70ed6434
Nam <doanngocnam3002@gmail.com>
2023-07-07 15:01:56 +0700

    type filter

--- airbnb-client/src/component/typeFilters/type.jsx
+++ airbnb-client/src/component/typeFilters/type.jsx
@@ -1,11 +1,83 @@
 import style from "./type.module.css";
+import { FaUmbrellaBeach, FaSwimmingPool } from "react-icons/fa";
+import {
+  GiBirchTrees,
+  GiCastleRuins,
+  GiForestCamp,
+  GiHomeGarage,
+  GiTreehouse,
+  GiParkBench,
+} from "react-icons/gi";
 
-export default function Type(props) {
-  const { icon, name } = props;
+export default function Type() {
+  const typeList = [
+    {
+      icon: FaUmbrellaBeach,
+      describe: "Beach view",
+      isFocus: 1,
+    },
+    {
+      icon: FaSwimmingPool,
+      describe: "Pool",
+      isFocus: 0,
+    },
+    {
+      icon: GiCastleRuins,
+      describe: "Castles",
+      isFocus: 0,
+    },
+    {
+      icon: GiForestCamp,
+      describe: "Camping",
+      isFocus: 0,
+    },
+    {
+      icon: GiHomeGarage,
+      describe: "Car park",
+      isFocus: 0,
+    },
+    {
+      icon: GiParkBench,
+      describe: "Park",
+      isFocus: 0,
+    },
+    {
+      icon: GiTreehouse,
+      describe: "Tree House",
+      isFocus: 0,
+    },
+    {
+      icon: GiBirchTrees,
+      describe: "Trees",
+      isFocus: 0,
+    },
+    {
+      icon: GiTreehouse,
+      describe: "Tree House",
+      isFocus: 0,
+    },
+    {
+      icon: GiBirchTrees,
+      describe: "Trees",
+      isFocus: 0,
+    },
+  ];
+
+  const renderType = (type, index) => {
+    const Icon = type.icon;
+    return (
+      <div className={style.type} key={index}>
+        <Icon />
+        <p>{type.describe}</p>
+      </div>
+    );
+  };
 
   return (
     <>
-      <div className={style.container}></div>
+      <div className={style.container}>
+        {typeList.map((type, index) => renderType(type, index))}
+      </div>
     </>
   );
 }
