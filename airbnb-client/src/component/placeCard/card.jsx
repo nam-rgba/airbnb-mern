310dd6904b71d6a12bb76d70469834e294d8e737
Nam <doanngocnam3002@gmail.com>
2023-07-10 17:09:05 +0700

    render list places

--- airbnb-client/src/component/placeCard/card.jsx
+++ airbnb-client/src/component/placeCard/card.jsx
@@ -1,28 +1,30 @@
 import style from "./card.module.css";
 import Image from "../../Image.jsx";
+import { AiFillStar } from "react-icons/ai";
 
-export default function Card() {
+export default function Card({ star }) {
   return (
-    <>
-      <div className={style.container}>
-        <div className={style.images}>
-          <div className={style.mark}>
-            <div className={style.slider}>
-              <Image name="temp" type="webp" />
-            </div>
-          </div>
-        </div>
-
-        <div className={style.infomation}>
-          <div className={style.position}>Tambon Pha Nok Khao, Thailand</div>
-          <div className={style.distance}>620 kilometers away</div>
-          <div className={style.date}>Jul 15-20</div>
-          <div className={style.price}>
-            <span>$26</span> night
+    <div className={style.container}>
+      <div className={style.images}>
+        <div className={style.mark}>
+          <div className={style.slider}>
+            <Image name="temp" type="webp" />
           </div>
         </div>
       </div>
-      ;
-    </>
+
+      <div className={style.infoma}>
+        <div className={style.position}>Tambon Pha Nok Khao, Thailand</div>
+        <div className={style.distance}>620 kilometers away</div>
+        <div className={style.date}>Jul 15-20</div>
+        <div className={style.price}>
+          <span>$26</span> nights
+        </div>
+        <span>
+          {" "}
+          <AiFillStar /> {star}
+        </span>
+      </div>
+    </div>
   );
 }
--- airbnb-client/src/pages/home/home.jsx
+++ airbnb-client/src/pages/home/home.jsx
@@ -1,13 +1,20 @@
 import Header from "../../component/header/index";
 import Type from "../../component/typeFilters/type";
 import Card from "../../component/placeCard/card";
+import style from "./home.module.css";
 
 const Home = () => {
+  const stars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  console.log(stars);
   return (
     <>
       <Header hide="" />
       <Type />
-      <Card />
+      <div className={style.places}>
+        {stars.map((star) => (
+          <Card star={star} key={star} />
+        ))}
+      </div>
     </>
   );
 };
