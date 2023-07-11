45d3e39865baa9b8c0014de2427ddbe62ba1e0a2
Nam <doanngocnam3002@gmail.com>
2023-07-11 22:34:50 +0700

    fix overlay bug

--- airbnb-client/src/component/placeCard/card.jsx
+++ airbnb-client/src/component/placeCard/card.jsx
@@ -5,15 +5,13 @@ import { AiFillStar } from "react-icons/ai";
 export default function Card({ star }) {
   return (
     <div className={style.container}>
-      <div className={style.images}>
-        <div className={style.mark}>
-          <div className={style.slider}>
-            <Image name="temp" type="webp" />
-          </div>
+      <div className={style.mark}>
+        <div className={style.slider}>
+          <Image name="temp" type="webp" />
         </div>
       </div>
 
-      <div className={style.infoma}>
+      <div className={style.infomation}>
         <div className={style.position}>Tambon Pha Nok Khao, Thailand</div>
         <div className={style.distance}>620 kilometers away</div>
         <div className={style.date}>Jul 15-20</div>
--- airbnb-client/src/pages/home/home.jsx
+++ airbnb-client/src/pages/home/home.jsx
@@ -4,7 +4,7 @@ import Card from "../../component/placeCard/card";
 import style from "./home.module.css";
 
 const Home = () => {
-  const stars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const stars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   console.log(stars);
   return (
     <>
