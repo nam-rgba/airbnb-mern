2f4ec24f23f4a331af1c734c170969f022d1762c
Nam <doanngocnam3002@gmail.com>
2023-07-17 23:58:36 +0700

    menu ui

--- airbnb-client/src/pages/hosting/hosting.jsx
+++ airbnb-client/src/pages/hosting/hosting.jsx
@@ -22,6 +22,26 @@ export default function Hosting() {
 
           <button className={style.menusub}>
             Menu <AiOutlineCaretDown />
+            <div className={style.menuexpand}>
+              <div className={style.menucontainer}>
+                <div className={style.part1}>
+                  <div>
+                    Get early access <span>new</span>
+                  </div>
+                </div>
+                <div className={style.part}>
+                  <div>Listing</div>
+                  <div>Reversation</div>
+                  <div>Creat a new listing</div>
+                </div>
+                <div className={style.part}>
+                  <div>Guidebooks</div>
+                  <div>Transaction history</div>
+                  <div>Explore hosting resources</div>
+                  <div>Connect with hosts near you</div>
+                </div>
+              </div>
+            </div>
           </button>
         </div>
         <div className={style.account}>
