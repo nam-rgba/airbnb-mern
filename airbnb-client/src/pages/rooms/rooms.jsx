f20e87a40d65f4bc5ba5c25712282368d4f1adc1
Nam <doanngocnam3002@gmail.com>
2023-08-31 22:19:35 +0700

    image grid

--- airbnb-client/src/pages/rooms/rooms.jsx
+++ airbnb-client/src/pages/rooms/rooms.jsx
@@ -7,9 +7,11 @@ import { FiShare } from "react-icons/fi";
 import { BsFillHouseHeartFill } from "react-icons/bs";
 import Image from "../../Image";
 import { useParams } from "react-router-dom";
+import data from "../../assets/fakedata/places.json";
 
 export default function Room() {
   const id = useParams();
+  const place = data.place[1];
   console.log(id);
   return (
     <div className={style.container}>
@@ -24,9 +26,11 @@ export default function Room() {
                 <div className={style.star}>
                   <AiFillStar /> 5.0
                 </div>
+                <div className="">.</div>
                 <div className={style.host_reward}>
                   <BiMedal /> Superhost
                 </div>
+                <div className="">.</div>
                 <div className={style.place}>
                   North Male Athon, Kaafu, Maldives
                 </div>
@@ -35,15 +39,35 @@ export default function Room() {
 
             <div className={style.action}>
               <div className={style.share}>
-                <FiShare /> Share
+                <FiShare />
+                Share
               </div>
               <div className={style.save}>
-                <BsFillHouseHeartFill /> Save
+                <BsFillHouseHeartFill />
+                Save
               </div>
             </div>
           </div>
 
-          <div className={style.photos}></div>
+          <div className={style.photos}>
+            <div className={style.big}>
+              <img src={place.image[0]} alt="" />
+            </div>
+            <div className={style.small}>
+              <div>
+                <img src={place.image[1]} alt="" />
+              </div>
+              <div>
+                <img src={place.image[2]} alt="" />
+              </div>
+              <div>
+                <img src={place.image[3]} alt="" />
+              </div>
+              <div>
+                <img src={place.image[0]} alt="" />
+              </div>
+            </div>
+          </div>
         </section>
 
         <section className={style.detail_and_payment}>
