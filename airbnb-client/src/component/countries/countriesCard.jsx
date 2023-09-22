26207b0da6e386e79c289700364a6c64d29a419e
Nam <doanngocnam3002@gmail.com>
2023-09-22 23:54:11 +0700

    tabs styles

--- airbnb-client/src/component/countries/countriesCard.jsx
+++ airbnb-client/src/component/countries/countriesCard.jsx
@@ -1,10 +1,14 @@
 import style from "./countriesCard.module.css";
+import Image from "../../Image";
 
 export default function CountriesCard({ props }) {
   const { name, map } = props;
   return (
     <div className={style.cardContainer}>
-      <div className={style.map}></div>
+      <div className={style.map}>
+        <Image name={map} type="webp" />
+      </div>
+      <div className={style.name}>{name}</div>
     </div>
   );
 }
--- airbnb-client/src/component/searchTab/Tabs/where.jsx
+++ airbnb-client/src/component/searchTab/Tabs/where.jsx
@@ -1,3 +1,9 @@
+import style from "./tabs.module.css";
 export default function Where() {
-  return <div>where</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.search_input}></div>
+      <div className={style.result}></div>
+    </div>
+  );
 }
