59e94a13d9ffc358d9e04debf3c284ce60aff971
Nam <doanngocnam3002@gmail.com>
2023-09-25 23:54:38 +0700

    cad palce

--- airbnb-client/src/component/searchTab/SearchTabs.jsx
+++ airbnb-client/src/component/searchTab/SearchTabs.jsx
@@ -1,5 +1,6 @@
 import style from "./searchTab.module.css";
 import { useState, lazy, Suspense } from "react";
+import { BiSearchAlt } from "react-icons/bi";
 
 export default function SearchTab() {
   const [active, setActive] = useState("where");
@@ -13,10 +14,18 @@ export default function SearchTab() {
     <div className={style.container}>
       <div className={style.tabs}>
         {bars.map((bar) => (
-          <div key={bar} onClick={() => changeTabs(bar)}>
+          <div
+            key={bar}
+            onClick={() => changeTabs(bar)}
+            className={bar == active ? style.tab_act : style.tab_off}
+          >
             {bar}
           </div>
         ))}
+        <div className={style.search_button}>
+          <p>Search</p>
+          <BiSearchAlt color="#ffffff" size={20} />
+        </div>
       </div>
       <div className={style.outlet}>
         <Suspense fallback={<p></p>}>
--- airbnb-client/src/component/searchTab/Tabs/where.jsx
+++ airbnb-client/src/component/searchTab/Tabs/where.jsx
@@ -1,8 +1,11 @@
 import style from "./tabs.module.css";
+
 export default function Where() {
   return (
     <div className={style.container}>
-      <div className={style.search_input}></div>
+      <div className={style.search_input}>
+        <input type="text" placeholder="Somewhere only we know..." />
+      </div>
       <div className={style.result}></div>
     </div>
   );
