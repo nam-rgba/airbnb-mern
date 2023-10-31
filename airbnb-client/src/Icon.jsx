536b44c5bf6609ba4794e633d70e8579189ff725
Nam <doanngocnam3002@gmail.com>
2023-10-31 23:48:43 +0700

    charactoeer component

--- airbnb-client/src/Icon.jsx
+++ airbnb-client/src/Icon.jsx
@@ -3,9 +3,11 @@ import { IconContext } from "react-icons";
 
 const DynamicIcon = ({ props }) => {
   const [library, icon] = props.icon.split("/");
+
   if (!library || !icon) {
     return <div>Cannot find icon</div>;
   }
+
   const path = `react-icons/${library}`;
   const Icon = lazy(async () => {
     const module = await import(path);
---
+++ airbnb-client/src/component/character/character.jsx
@@ -0,0 +1,26 @@
+import style from "./character.module.css";
+import { lazy } from "react";
+
+export default function Character({ props }) {
+  const { icon, name, describe } = props;
+
+  const lazyIcons = {
+    Door: lazy(() => import("react-icons/bs/BsDoorOpen")),
+    Medal: lazy(() => import("react-icons/pi/PiMedalDuotone")),
+    Location: lazy(() => import("react-iocns/sl/SlLocationPin")),
+  };
+
+  const Icon = lazyIcons[icon];
+
+  return (
+    <div className={style.container}>
+      <div className={style.icon}>
+        <Icon />
+      </div>
+      <div className={style.info}>
+        <div className={style.name}>{name}</div>
+        <div className={style.describe}>{describe}</div>
+      </div>
+    </div>
+  );
+}
