3ae3a17d6a60092a384666c1b7d1bf9723e640dc
Nam <doanngocnam3002@gmail.com>
2024-04-08 17:43:00 +0700

    update(logic): count time

--- airbnb-client/src/pages/user/profile/profile.jsx
+++ airbnb-client/src/pages/user/profile/profile.jsx
@@ -1,9 +1,9 @@
-import style from "./profile.module.css";
-import { useContext } from "react";
-import { UserContext } from "../../../contexts/UserContext";
-import { useNavigate, Link } from "react-router-dom";
-import Image from "../../../Image";
-import { MdOutlineDone } from "react-icons/md";
+import style from './profile.module.css';
+import { useContext } from 'react';
+import { UserContext } from '../../../contexts/UserContext';
+import { useNavigate } from 'react-router-dom';
+import { MdOutlineDone, MdOutlineQuestionMark } from 'react-icons/md';
+import avt from '../../../assets/logo/airbnb.svg';
 
 export default function ProfilePage() {
   const { user, ready } = useContext(UserContext);
@@ -13,12 +13,13 @@ export default function ProfilePage() {
     const timeCreate = Date.parse(user.timeCreate);
     const totals = now - timeCreate;
     console.log(now);
-    console.log(timeCreate);
+    console.log(user.timeCreate);
+
     return Math.floor(totals / 86400000) + 1;
   };
 
   if (ready && !user) {
-    return navigate("/account/login");
+    return navigate('/account/login');
   } else if (!ready) {
     return <p>Loading...</p>;
   }
@@ -29,43 +30,39 @@ export default function ProfilePage() {
         <section className={style.card}>
           <div className={style.card_info}>
             <div className={style.avt}>
-              <Image name="profile" type="jpeg" />
-              <h3>{user.name}</h3>
-              <p>Guest</p>
+              <img src={avt} alt="" />
             </div>
             <div className={style.countTime}>
-              <span>{time()}</span>
-              <p> days on Airbnb!</p>{" "}
+              <div>
+                <h3>{user.name}</h3>
+                <p>Guest</p>
+              </div>
+              <div>
+                <span>{time()}</span>
+                <p> days on Airbnb!</p>{' '}
+              </div>
             </div>
           </div>
           <div className={style.verification}>
             <div className={style.email}>
               <h3>Your confirmed infomation </h3>
               <span>
-                <MdOutlineDone />
+                <MdOutlineDone color="green" size={20} />
                 <p>Email address</p>
               </span>
             </div>
-            <div className={style.identify}>
+            <div className={style.email}>
               <h3>Verify your identity </h3>
 
-              <p>
-                Before you book or Host on Airbnb, you’ll need to complete this
-                step.
-              </p>
+              <span>
+                <MdOutlineQuestionMark size={28} color="#f9375a" />
+                <p>
+                  Before you book or Host on Airbnb, you’ll need to complete
+                  this step
+                </p>
+              </span>
             </div>
           </div>
-        </section>
-
-        <section className={style.create_profile}>
-          <h2> It is time to create your profile </h2>
-          <p>
-            Your Airbnb profile is an important part of every reservation.
-            Create yours to help other Hosts and guests get to know you.
-          </p>
-          <button>
-            <Link>Create profile</Link>
-          </button>
         </section>
       </div>
 
