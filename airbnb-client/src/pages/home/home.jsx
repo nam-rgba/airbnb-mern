5e2b6ff155c6eb2d61c21410dd5583ad7c52d09c
Nam <doanngocnam3002@gmail.com>
2024-04-10 00:03:11 +0700

    define bug

--- airbnb-client/src/pages/home/home.jsx
+++ airbnb-client/src/pages/home/home.jsx
@@ -2,13 +2,25 @@ import Header from '../../component/header/index';
 import Type from '../../component/typeFilters/type';
 import Card from '../../component/placeCard/card';
 import style from './home.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useFilter } from '../../hooks/useFilter';
 
 const Home = () => {
   const [isSearchDisplay, setIsSearchDisplay] = useState(false);
   let { placeFiltered, handleType, handleSearch } = useFilter();
 
+  const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    axios
+      .get('/places')
+      .then((res) => {
+        setRooms(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   console.log(placeFiltered);
 
   const whenDisplaySearch = () => {
@@ -34,7 +46,7 @@ const Home = () => {
           <Type handleType={handleType} />
         </div>
         <div className={style.places}>
-          {placeFiltered.map((place) => (
+          {rooms.map((place) => (
             <Card key={place.place} place={place} />
           ))}
         </div>
