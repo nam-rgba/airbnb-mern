5426c6caac8944e8c105888bb54fea67f8da4855
Nam <doanngocnam3002@gmail.com>
2024-04-10 00:23:39 +0700

    last hope

--- airbnb-client/src/pages/home/home.jsx
+++ airbnb-client/src/pages/home/home.jsx
@@ -2,25 +2,13 @@ import Header from '../../component/header/index';
 import Type from '../../component/typeFilters/type';
 import Card from '../../component/placeCard/card';
 import style from './home.module.css';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
 import { useFilter } from '../../hooks/useFilter';
 
 const Home = () => {
   const [isSearchDisplay, setIsSearchDisplay] = useState(false);
   let { placeFiltered, handleType, handleSearch } = useFilter();
 
-  const [rooms, setRooms] = useState([]);
-  useEffect(() => {
-    axios
-      .get('/places')
-      .then((res) => {
-        setRooms(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
   console.log(placeFiltered);
 
   const whenDisplaySearch = () => {
@@ -46,7 +34,7 @@ const Home = () => {
           <Type handleType={handleType} />
         </div>
         <div className={style.places}>
-          {rooms.map((place) => (
+          {placeFiltered.map((place) => (
             <Card key={place.place} place={place} />
           ))}
         </div>
